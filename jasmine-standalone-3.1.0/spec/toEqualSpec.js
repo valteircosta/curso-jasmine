40dc51c3a44510df4deca3614b149d30a02b6474
Valteir Costa <valteircosta@gmail.com>
2018-06-13 21:11:01 -0300

    incluido toEqual

--- jasmine-standalone-3.1.0/spec/toEqualSpec.js
+++ jasmine-standalone-3.1.0/spec/toEqualSpec.js
@@ -1,10 +1,15 @@
+describe("Comparador toEqual", function () {
 
-describe('Comparador toEqual', function() {
-   
-   it('deve validar o uso do toEqual', function() {
-      expect(true).toEqual(true);
-           
-   });
-        
+    it("deve validar o uso do 'toEqual'", function () {
+        expect(true).toEqual(true);
+        let obj1 = {
+            valor: true
+        };
+        let obj2 = {
+            valor: true
+        };
+        expect(obj1).toEqual(obj2);
+
+    });
+
 });
-    
